3601f18e3443b62124b3066ffaf65e0342eee36a
0marsaad <es-omar.adel2026@alexu.edu.eg>
2023-12-26 03:00:48 +0200

    first commit

---
+++ src/App.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+
+public class App extends JFrame {
+    
+    void Frame1() {
+        JFrame frame = new JFrame("Tic Taco Toe ");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 300);
+        frame.setVisible(true);
+    }
+    public static void main(String[] args) throws Exception {
+
+
+        
+    }
+}
